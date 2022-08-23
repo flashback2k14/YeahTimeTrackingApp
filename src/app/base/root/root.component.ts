e0faf5f5c6a68eaa2d0379ffd25ed4b7e9abc4f4
Sebastian Kloppe <yeahdev@gmail.com>
2022-08-23 12:49:29 +0000

    add header, main and footer area

--- src/app/base/root/root.component.ts
+++ src/app/base/root/root.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+import { CommonModule } from '@angular/common';
+
+type Link = {
+  href: string;
+  title: string;
+};
+
 @Component({
   selector: 'ytt-root',
   standalone: true,
-  imports: [RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './root.component.html',
   styleUrls: ['./root.component.scss'],
 })
 export class RootComponent {
-  title = 'YeahTimeTrackingApp';
+  title = 'Yeah! Time tracking';
+
+  links: Link[];
+
+  constructor() {
+    this.links = [
+      { href: 'dashboard', title: 'Dashboard' },
+      { href: 'settings', title: 'Settings' },
+    ];
+  }
 }
