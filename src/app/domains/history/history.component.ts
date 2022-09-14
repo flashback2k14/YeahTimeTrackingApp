5816ae1958b2839fef60dac2f835f668a215d25a
Sebastian Kloppe <yeahdev@gmail.com>
2022-09-14 13:38:20 +0000

    start implementing history component

--- src/app/domains/history/history.component.ts
+++ src/app/domains/history/history.component.ts
@@ -1,18 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { MatTableModule } from '@angular/material/table';
+
+import { HistoryResponse, HistoryTask, HistoryItem } from './models';
+import { HttpService } from 'src/app/core/http.service';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import {
+  catchError,
+  map,
+  merge,
+  of,
+  startWith,
+  switchMap,
+  throwError,
+} from 'rxjs';
 
 @Component({
   selector: 'ytt-history',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule, MatPaginatorModule, MatSortModule],
   templateUrl: './history.component.html',
-  styleUrls: ['./history.component.scss']
+  styleUrls: ['./history.component.scss'],
 })
-export class HistoryComponent implements OnInit {
+export class HistoryComponent implements AfterViewInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
-  constructor() { }
+  displayedColumns: string[] = ['modified', 'state', 'duration', 'comment'];
+  data: HistoryItem[] = [];
+  resultsLength = 0;
 
-  ngOnInit(): void {
+  constructor(private _httpService: HttpService) {}
+
+  ngAfterViewInit(): void {
+    // If the user changes the sort order, reset back to the first page.
+    this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
+
+    merge(this.sort.sortChange, this.paginator.page)
+      .pipe(
+        startWith({}),
+        switchMap(() => {
+          // TODO: API / notion only returns 100 items
+          return this._httpService
+            .get<HistoryResponse>('/history')
+            .pipe(catchError(() => of(null)));
+        }),
+        map((data: HistoryResponse | null) => {
+          // Flip flag to show that loading has finished.
+          if (data === null) {
+            return [];
+          }
+
+          // Only refresh the result length if there is new data. In case of rate
+          // limit errors, we do not want to reset the paginator to zero, as that
+          // would prevent users from re-triggering requests.
+          this.resultsLength = data.historyTasks.length;
+          return data.historyTasks;
+        })
+      )
+      .subscribe(
+        (data: HistoryTask[]) =>
+          (this.data = data.map((task: HistoryTask) =>
+            HistoryItem.create(task)
+          ))
+      );
   }
-
 }
