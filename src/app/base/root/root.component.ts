47a2b599489299997e668686214a9a362bb9bf97
Sebastian Kloppe <sebastian.kloppe@smartcommerce.de>
2023-10-24 16:23:10 +0200

    initial commit

--- src/app/base/root/root.component.ts
+++ src/app/base/root/root.component.ts
@@ -1,5 +1,5 @@
 import { DomSanitizer } from '@angular/platform-browser';
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { MatIconRegistry } from '@angular/material/icon';
@@ -14,95 +14,92 @@ type Link = {
 
 @Component({
   selector: 'ytt-root',
-  standalone: true,
-  imports: rootComponentModules,
   templateUrl: './root.component.html',
   styleUrls: ['./root.component.scss'],
+  standalone: true,
+  imports: rootComponentModules,
 })
 export class RootComponent {
-  title: string;
-  links: Link[];
+  private readonly router = inject(Router);
+  private readonly iconRegistry = inject(MatIconRegistry);
+  private readonly sanitizer = inject(DomSanitizer);
 
-  constructor(
-    protected authService: AuthService,
-    private _router: Router,
-    private _iconRegistry: MatIconRegistry,
-    private _sanitizer: DomSanitizer
-  ) {
-    this.title = 'Yeah! Time tracking';
-    this.links = [
-      { href: 'dashboard', title: 'Dashboard' },
-      { href: 'history', title: 'History' },
-      { href: 'settings', title: 'Settings' },
-    ];
+  protected authService = inject(AuthService);
+  protected title = signal('Yeah! Time tracking');
+  protected links = signal<Link[]>([
+    { href: 'dashboard', title: 'Dashboard' },
+    { href: 'history', title: 'History' },
+    { href: 'settings', title: 'Settings' },
+  ]);
 
+  constructor() {
     this._initIcons();
   }
 
   goToHome(): void {
-    this._router.navigate([this.links[0].href]);
+    this.router.navigate([this.links()[0].href]);
   }
 
   private _initIcons(): void {
-    this._iconRegistry.addSvgIcon(
+    this.iconRegistry.addSvgIcon(
       'download',
-      this._sanitizer.bypassSecurityTrustResourceUrl(
+      this.sanitizer.bypassSecurityTrustResourceUrl(
         '../assets/svg/cloud-download-outline.svg'
       )
     );
 
-    this._iconRegistry.addSvgIcon(
+    this.iconRegistry.addSvgIcon(
       'upload',
-      this._sanitizer.bypassSecurityTrustResourceUrl(
+      this.sanitizer.bypassSecurityTrustResourceUrl(
         '../assets/svg/cloud-upload-outline.svg'
       )
     );
 
-    this._iconRegistry.addSvgIcon(
+    this.iconRegistry.addSvgIcon(
       'save',
-      this._sanitizer.bypassSecurityTrustResourceUrl(
+      this.sanitizer.bypassSecurityTrustResourceUrl(
         '../assets/svg/content-save-outline.svg'
       )
     );
 
-    this._iconRegistry.addSvgIcon(
+    this.iconRegistry.addSvgIcon(
       'no-data',
-      this._sanitizer.bypassSecurityTrustResourceUrl(
+      this.sanitizer.bypassSecurityTrustResourceUrl(
         '../assets/svg/database-off-outline.svg'
       )
     );
 
-    this._iconRegistry.addSvgIcon(
+    this.iconRegistry.addSvgIcon(
       'delete',
-      this._sanitizer.bypassSecurityTrustResourceUrl(
+      this.sanitizer.bypassSecurityTrustResourceUrl(
         '../assets/svg/delete-outline.svg'
       )
     );
 
-    this._iconRegistry.addSvgIcon(
+    this.iconRegistry.addSvgIcon(
       'edit',
-      this._sanitizer.bypassSecurityTrustResourceUrl(
+      this.sanitizer.bypassSecurityTrustResourceUrl(
         '../assets/svg/pencil-outline.svg'
       )
     );
 
-    this._iconRegistry.addSvgIcon(
+    this.iconRegistry.addSvgIcon(
       'play',
-      this._sanitizer.bypassSecurityTrustResourceUrl(
+      this.sanitizer.bypassSecurityTrustResourceUrl(
         '../assets/svg/timer-play-outline.svg'
       )
     );
 
-    this._iconRegistry.addSvgIcon(
+    this.iconRegistry.addSvgIcon(
       'stop',
-      this._sanitizer.bypassSecurityTrustResourceUrl(
+      this.sanitizer.bypassSecurityTrustResourceUrl(
         '../assets/svg/timer-stop-outline.svg'
       )
     );
 
-    this._iconRegistry.addSvgIcon(
+    this.iconRegistry.addSvgIcon(
       'reload',
-      this._sanitizer.bypassSecurityTrustResourceUrl('../assets/svg/reload.svg')
+      this.sanitizer.bypassSecurityTrustResourceUrl('../assets/svg/reload.svg')
     );
   }
 }
